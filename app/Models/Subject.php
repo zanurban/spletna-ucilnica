cfc36fbe736fa8f61dbd4a5562ed845fd7d93d45
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-09-05 21:03:11 +0200

    adding seeder and factory for subject and users

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
+    use HasFactory;
+    use HasUuids;
+
     /**
      * The primary key for the model.
      *
