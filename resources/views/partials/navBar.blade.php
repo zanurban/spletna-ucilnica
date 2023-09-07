c374799d5cd867715169d6eadd24561c1155c1ff
zanurban <zan.skorja@gmail.com>
2023-09-07 18:34:48 +0200

    Updated styling

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -1,5 +1,8 @@
-@section('navBar')
-    <div class="navbar">
 
+    <div class="sidebar">
+        <a href="#" class="active">Button 1</a>
+        <a href="#">Button 2</a>
+        <a href="#">Button 3</a>
+        <a href="#">Button 4</a>
+        <a href="#">Button 5</a>
     </div>
-@endsection
