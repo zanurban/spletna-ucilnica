c998229f81e7771a9649f58177b427511fb14f0c
zanurban <zan.skorja@gmail.com>
2023-09-11 22:49:25 +0200

    Styling loginForm

---
+++ resources/views/components/login.blade.php
@@ -0,0 +1,22 @@
+<div class="frame">
+    <div class="container-fluid loginForm">
+        <div class="form">
+            <form>
+                <div class="box">
+                    Ime
+                    <input type="username">
+                </div>
+                <div class="box">
+                    Geslo
+                    <input type="password">
+                </div>
+                <div class="box-center">
+                    <input type="submit">
+                </div>
+            </form>
+        </div>
+        <div class="image">
+            <img src="">
+        </div>
+    </div>
+</div>
