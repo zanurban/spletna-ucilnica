4eaf3c4970ddbd74e92bddb3d83585c705612882
zanurban <zan.skorja@gmail.com>
2023-09-12 11:26:57 +0200

    Update navBar.blade.php

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -1,7 +1,3 @@
 <div class="sidebar">
-    @if (count($data) > 0)
-        @foreach ($data as $row)
-            <a href="#">{{ $row->subject_name }}</a>
-        @endforeach
-    @endif
+   
 </div>
