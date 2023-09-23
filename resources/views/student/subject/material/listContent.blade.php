36fa9385a53fbc0a9a9a4f712e3643b3f6860ddb
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-09-23 20:22:14 +0200

    bug fixes and submission buttons redesign

--- resources/views/student/subject/material/listContent.blade.php
+++ resources/views/student/subject/material/listContent.blade.php
@@ -16,7 +16,8 @@
                 @if (count($materials) > 0)
                     @foreach ($materials as $row)
                         <tr>
-                            <td><a href="{{ Storage::url($row->material_file_path) }}">{{ $row->material_title }}</a></td>
+                            <td><a href="{{ Storage::url($row->material_file_path) }}">{{ $row->material_title }}</a>
+                            </td>
                             <td>{{ $row->material_description }}</td>
                         </tr>
                     @endforeach
@@ -38,34 +39,27 @@
                 @if (count($assignments) > 0)
                     @foreach ($assignments as $row)
                         <tr>
-                            <td>{{ $row->assignment_title }}</td>
-                            <td>{{ $row->assignment_description }}</td>
-                            <td>{{ $row->completion_date }}</td>
+                            <td>{{ $row?->assignment_title }}</td>
+                            <td>{{ $row?->assignment_description }}</td>
+                            <td>{{ $row?->completion_date }}</td>
                             <td>
-                                @if ($row->material_file_path !== '')
-                                    <a href="{{ Storage::url($row->material_file_path) }}">{{ $row->assignment_title }}</a>
+                                @if ($row?->material_file_path !== '')
+                                    <a href="{{ Storage::url($row?->material_file_path) }}">{{ $row?->assignment_title }}</a>
                                 @endif
                             </td>
                             <td>
-                                <form
-                                    action="{{ route('assigment.show', ['subjectId' => $subjectId, 'assignmentId' => $row?->id]) }}"
-                                    method="POST">
-                                    @method('GET')
-                                    @csrf
-                                    <div class="d-flex gap-2">
-                                        <button type="submit" class="btn btn-primary btn-sm add">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                            width="16" height="16" fill="currentColor" class="bi bi-pencil-square"
-                                            viewBox="0 0 16 16">
-                                           <path
-                                               d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                           <path fill-rule="evenodd"
-                                                 d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                       </svg>
-                                            Oddaj nalogo
-                                        </button>
-                                    </div>
-                                </form>
+                                <div class="d-flex gap-2">
+                                    <a href="{{ route('assignment.show', ['subjectId' => $subjectId, 'assignmentId' => $row?->id]) }}"
+                                       class="btn btn-primary btn-sm add">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                        </svg>
+                                        Oddaj nalogo</a>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
