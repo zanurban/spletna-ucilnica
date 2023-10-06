cd3ea2a7c975a8c474e093b04882fdbc28caa51d
zanurban <zan.skorja@gmail.com>
2023-10-06 22:45:33 +0200

    Getting changes ready for server

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,10 +1,17 @@
-<form
-    class="form"
-    action="{{ isset($existingData->id) ? route($submitRouteName . '.update', [$variableName => $existingData->id, $optionalVariableName => $optionalId]) : route($submitRouteName . '.create', [$optionalVariableName => $optionalId]) }}"
-    method="POST"
-    enctype="multipart/form-data">
+<form class="form"
+    action="{{ isset($existingData->id)
+        ? (isset($optionalVariableName)
+            ? route($submitRouteName . '.update', [
+                $variableName => $existingData->id,
+                $optionalVariableName => $optionalId,
+            ])
+            : route($submitRouteName . '.update', [$variableName => $existingData->id]))
+        : (isset($optionalVariableName)
+            ? route($submitRouteName . '.create', [$optionalVariableName => $optionalId])
+            : route($submitRouteName . '.create')) }}"
+    method="POST" enctype="multipart/form-data">
     @csrf
-    @if(isset($existingData->id))
+    @if (isset($existingData->id))
         @method('PUT')
     @endif
 
@@ -12,6 +19,11 @@
         {{ $slot }}
     </div>
 
-    <button type="submit" class="btn btn-primary">{{ $submitButtonName }}</button>
-    <a href="{{ route($backRouteName, [$optionalVariableName => $optionalId]) }}" class="btn btn-default" style="margin-left: 10px">Nazaj</a>
+    <button type="submit" class="btn btn-primary">{{ $submitButtonName ?? 'Shrani' }}</button>
+    <a href="{{ 
+        isset($optionalVariableName) 
+        ? route($backRouteName, [$optionalVariableName => $optionalId]) 
+        : route($backRouteName) 
+    }}" class="btn btn-default" style="margin-left: 10px">Nazaj</a>
+    
 </form>
